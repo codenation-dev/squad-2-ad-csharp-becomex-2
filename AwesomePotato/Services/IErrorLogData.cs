941d055ac24f39b169d4f0aa9928476ce9bfa9fa
Luiz Soares <dev.soares@outlook.com>
2020-04-03 20:08:57 -0300

    Added IErrorLogData

---
+++ AwesomePotato/Services/IErrorLogData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AwesomePotato.Models;
+
+namespace AwesomePotato.Services
+{
+    public interface IErrorLogData
+    {
+        ErrorLogData FindById(int id);
+        IList<ErrorLogData> FindByAplicationName(string name);
+        IList<ErrorLogData> FindByToken(Guid token);
+        ErrorLogData Save(ErrorLogData errorLogData);
+    }
+}
