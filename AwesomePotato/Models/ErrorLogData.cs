890a2d68c4aaf7467993986b3f4be2fd0c008b96
Luiz Soares <dev.soares@outlook.com>
2020-04-03 19:39:37 -0300

    Added ErrorLogData class

---
+++ AwesomePotato/Models/ErrorLogData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwesomePotato.Models
+{
+    public class ErrorLogData
+    {
+        [Key]
+        public int Id { get; private set; }
+        public Guid Token { get; set; }
+        public int Level { get; set; }
+        [StringLength(100)]
+        public string Aplication { get; set; }
+        [StringLength(300)]
+        public string Title { get; set; }
+        [StringLength(3000)]
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public DateTime TimeStamp { get; private set; }
+    }
+}
