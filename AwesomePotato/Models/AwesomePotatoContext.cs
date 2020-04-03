35fb8ddcc74b68f56a95bfd7bbafa7ddeb51338a
Luiz Soares <dev.soares@outlook.com>
2020-04-03 20:22:08 -0300

    Added AwesomePotatoContext.cs

---
+++ AwesomePotato/Models/AwesomePotatoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwesomePotato.Models
+{
+    public class AwesomePotatoContext : DbContext
+    {
+        public DbSet<ErrorLogData> ErrorLogData { get; set; }
+    }
+}
