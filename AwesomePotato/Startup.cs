d551c9d88667da27c19c8bcb4531c547776f4535
Luiz Soares <dev.soares@outlook.com>
2020-04-04 18:58:26 -0300

    Added AutoMapper and ErrorLogDataService to Startup.cs

--- AwesomePotato/Startup.cs
+++ AwesomePotato/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AwesomePotato.Models;
+using AwesomePotato.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using AutoMapper;
 
 namespace AwesomePotato
 {
@@ -25,7 +28,9 @@ namespace AwesomePotato
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddDbContext<AwesomePotatoContext>();            
+            services.AddAutoMapper(typeof(Startup));
+            services.AddScoped<IErrorLogDataService, ErrorLogDataService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
