b81180a7d55f1949266d7f7de457a6d2c715ba35
Luiz Soares <dev.soares@outlook.com>
2020-04-04 15:18:53 -0300

    Added ErroLogDataDTO.cs

---
+++ AwesomePotato/DTOs/ErroLogDataDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwesomePotato.DTOs
+{
+    public class ErroLogDataDTO
+    {
+        public int Id { get; private set; }
+        public Guid Token { get; set; }
+        public int Level { get; set; }
+        public string Aplication { get; set; }
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public DateTime TimeStamp { get; private set; }
+    }
+}
