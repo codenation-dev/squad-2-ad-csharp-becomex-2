9a2ff5bc7465c16fd3649b61b80c894b184eadcc
Luiz Soares <dev.soares@outlook.com>
2020-04-08 16:54:15 -0300

    Updated IErrorLogDataService structure

--- AwesomePotato/Services/IErrorLogDataService.cs
+++ AwesomePotato/Services/IErrorLogDataService.cs
@@ -9,14 +9,12 @@ namespace AwesomePotato.Services
     public interface IErrorLogDataService
     {
         ErrorLogData FindById(int id);
-        IList<ErrorLogData> FindByAplicationName(string name);
-        IList<ErrorLogData> FindByToken(Guid token);
-        IList<ErrorLogData> FindByLevel(int level);
-        IList<ErrorLogData> FindByAplicationNameAndLevel(string name, int level);
-        IList<ErrorLogData> FindByAplicationNameAndToken(string name, Guid token);
-        IList<ErrorLogData> FindByTokenAndLevel(Guid token, int level);
-        IList<ErrorLogData> FindByAplicationNameAndTokenAndLevel(string name, Guid token, int level);
-        IList<ErrorLogData> FindByDateTime(DateTime startDate, DateTime endDate);
+        IList<ErrorLogData> FilterByApplicationTokenLevelDate(string applicationName, string token, int? level, string startDate, string endDate);
+        IList<ErrorLogData> GetAll();
+        IList<ErrorLogData> FilterByApplication(IList<ErrorLogData> logDatas, string application);
+        IList<ErrorLogData> FilterByToken(IList<ErrorLogData> logDatas, string token);
+        IList<ErrorLogData> FilterByLevel(IList<ErrorLogData> logDatas, int? level);
+        IList<ErrorLogData> FilterByDate(IList<ErrorLogData> logDatas, string startDate, string endDate);
         ErrorLogData Save(ErrorLogData errorLogData);
     }
 }
