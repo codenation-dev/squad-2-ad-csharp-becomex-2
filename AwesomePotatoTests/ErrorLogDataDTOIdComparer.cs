cf064034571745a327dfe21b1cba42617b913662
Luiz Soares <dev.soares@outlook.com>
2020-04-09 19:28:57 -0300

    Added ErrorLogDataDTOIdComparer class and implemented interface methods

---
+++ AwesomePotatoTests/ErrorLogDataDTOIdComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using AwesomePotato.DTOs;
+using System.Text;
+
+namespace AwesomePotatoTests
+{
+    class ErrorLogDataDTOIdComparer : IEqualityComparer<ErrorLogDataDTO>
+    {
+        public bool Equals([AllowNull] ErrorLogDataDTO x, [AllowNull] ErrorLogDataDTO y)
+        {
+            return x.Id == y.Id;
+        }
+
+        public int GetHashCode([DisallowNull] ErrorLogDataDTO obj)
+        {
+            return obj.Id.GetHashCode();
+        }
+    }
+}
