47f89340798d7d9c10e6b3b3fb2c24b38966a346
Luiz Soares <dev.soares@outlook.com>
2020-04-16 20:36:09 -0300

    Added IdentityContext.cs to Infraestructure folder

---
+++ AwesomePotato/Infraestructure/IdentityContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AwesomePotato.Infraestructure
+{
+    public class IdentityContext : IdentityDbContext
+    {
+        public IdentityContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
