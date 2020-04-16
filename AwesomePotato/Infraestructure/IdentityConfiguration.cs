db0234aeaa8b86813bf495a312afc7da34d0de23
Luiz Soares <dev.soares@outlook.com>
2020-04-16 20:43:33 -0300

    Added class that configures IdentityContext

---
+++ AwesomePotato/Infraestructure/IdentityConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using AwesomePotato.DTOs;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace AwesomePotato.Infraestructure
+{
+    public static class IdentityConfiguration
+    {
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IdentityContext>(x => x.UseSqlServer(configuration.GetConnectionString("default")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                    .AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<IdentityContext>()
+                    .AddDefaultTokenProviders();
+
+            ConfigureToken(services, configuration);
+        }
+
+        private static void ConfigureToken(IServiceCollection services, IConfiguration configuration)
+        {
+            var appSettingsSection = configuration.GetSection("AppSettings");
+            services.Configure<AppSettingsDTO>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettingsDTO>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = true;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.Audience,
+                    ValidIssuer = appSettings.Issuer
+                };
+            });
+        }
+    }
+}
