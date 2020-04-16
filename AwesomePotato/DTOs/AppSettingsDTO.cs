1714a127ef2d408dd9998f881100b8ad9af576f5
Luiz Soares <dev.soares@outlook.com>
2020-04-16 20:40:44 -0300

    Added AppSettingsDTO.cs

---
+++ AwesomePotato/DTOs/AppSettingsDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace AwesomePotato.DTOs
+{
+    public class AppSettingsDTO
+    {
+        public string Secret { get; set; }
+        public int ExpirationTime { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+    }
+}
