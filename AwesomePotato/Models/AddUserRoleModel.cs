27f592681a9f5614d9852aded90c61b887d660ee
Luiz Soares <dev.soares@outlook.com>
2020-04-16 21:47:35 -0300

    Added AddUserRoleModel.cs

---
+++ AwesomePotato/Models/AddUserRoleModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AwesomePotato.Models
+{
+    public class AddUserRoleModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Role { get; set; }
+    }
+}
