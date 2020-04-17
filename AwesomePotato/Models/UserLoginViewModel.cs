6e909f292bae43b126a8daf2e628463c8a1e4360
Luiz Soares <dev.soares@outlook.com>
2020-04-16 22:57:03 -0300

    Added UserLoginViewModel.cs

---
+++ AwesomePotato/Models/UserLoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AwesomePotato.Models
+{
+    public class UserLoginViewModel
+    {
+        [Required(ErrorMessage = "O campo {0} é Obrigatório!")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é Obrigatório!")]
+        [StringLength(100, ErrorMessage = "O campo deve conter entre {2} e {1} caracteres", MinimumLength = 10)]
+        public string Senha { get; set; }
+    }
+}
