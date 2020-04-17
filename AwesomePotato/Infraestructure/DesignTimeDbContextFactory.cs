2b6fec096aab0c57404431f93ad60fd64c4d774d
Luiz Soares <dev.soares@outlook.com>
2020-04-16 21:45:46 -0300

    Added IdentityContext helper

---
+++ AwesomePotato/Infraestructure/DesignTimeDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace AwesomePotato.Infraestructure
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<IdentityContext>
+    {
+        public IdentityContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                                                        .SetBasePath(Directory.GetCurrentDirectory())
+                                                        .AddJsonFile("appsettings.json")
+                                                        .Build();
+
+            var builder = new DbContextOptionsBuilder<IdentityContext>();
+            var connectionString = configuration.GetConnectionString("default");
+            builder.UseSqlServer(connectionString);
+            return new IdentityContext(builder.Options);
+        }
+    }
+}
