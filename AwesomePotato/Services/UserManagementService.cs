2034156befef5cc7d74ba366fdfeab2acae09d55
Luiz Soares <dev.soares@outlook.com>
2020-04-17 13:43:38 -0300

    Implemented GetAllUserClaims method, and fixed GerarJWT method

--- AwesomePotato/Services/UserManagementService.cs
+++ AwesomePotato/Services/UserManagementService.cs
@@ -5,8 +5,10 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -49,20 +51,45 @@ namespace AwesomePotato.Services
                 result = await _userManager.CreateAsync(user, registroUsuario.Senha).ConfigureAwait(true);
 
                 if (result.Succeeded)
-                {
                     await _signInManager.SignInAsync(user, false).ConfigureAwait(true);
-                    await _userManager.AddToRoleAsync(user, "Usuario").ConfigureAwait(true);
-                }
+
             }
-            else
-                result.Errors.Append(new IdentityError { Description = $"Email: {registroUsuario.Email} já cadastrado." });
 
             return result;
         }
-
-
-        public string GerarJWT(string email)
+        public async Task<List<Claim>> GetAllUserClaims(IdentityUser user)
         {
+            var claims = new List<Claim>();
+
+            var resultRoles = await _userManager.GetRolesAsync(user).ConfigureAwait(true);
+            var roles = _roleManager.Roles.Where(x => resultRoles.Contains(x.Name)).ToList();
+
+            foreach (var role in roles)
+            {
+                var roleClaims = await _roleManager.GetClaimsAsync(role).ConfigureAwait(true);
+                claims.AddRange(roleClaims);
+            }
+
+            claims.AddRange(await _userManager.GetClaimsAsync(user).ConfigureAwait(true));
+
+            return claims;
+        }
+
+
+        public async Task<string> GerarJWT(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email).ConfigureAwait(true);
+
+            var identityClaims = new ClaimsIdentity();
+
+            identityClaims.AddClaims(await GetAllUserClaims(user).ConfigureAwait(true));
+
+            var roles = await _userManager.GetRolesAsync(user).ConfigureAwait(true);
+            foreach (var role in roles)
+            {
+                identityClaims.AddClaim(new Claim(ClaimTypes.Role, role));
+            }
+
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
 
@@ -73,6 +100,7 @@ namespace AwesomePotato.Services
                 Expires = DateTime.UtcNow.AddHours(_appSettings.ExpirationTime),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
                 SecurityAlgorithms.HmacSha256Signature),
+                Subject = identityClaims
             };
 
             return tokenHandler.WriteToken(tokenHandler.CreateToken(tokenDescriptor));
