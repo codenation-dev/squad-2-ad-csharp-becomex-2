00d2a6efcceecd9cdb31192f36c89b145341f547
Luiz Soares <dev.soares@outlook.com>
2020-04-16 23:05:38 -0300

    Added IUserManagementService.cs

---
+++ AwesomePotato/Services/IUserManagementService.cs
@@ -0,0 +1,15 @@
+﻿using AwesomePotato.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwesomePotato.Services
+{
+    public interface IUserManagementService
+    {
+        Task<IdentityResult> Create(UserRegisterViewModel registroUsuario);
+        string GerarJWT(string email);
+    }
+}
