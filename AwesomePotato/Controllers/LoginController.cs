ce5904c1a996ea6208eafc5e0cba53360ee6dd0c
Luiz Soares <dev.soares@outlook.com>
2020-04-16 22:58:16 -0300

    Fixed LoginController.cs

--- AwesomePotato/Controllers/LoginController.cs
+++ AwesomePotato/Controllers/LoginController.cs
@@ -1,11 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using AwesomePotato.Models;
+using AwesomePotato.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AwesomePotato.Controllers
 {
-    public class LoginController
+    public class LoginController : ControllerBase
     {
+        private readonly UserManagementService _userManagementService;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public LoginController(UserManagementService userManagementService,
+                                SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+            _userManagementService = userManagementService;
+        }
+
+        [HttpPost("Registrar")]
+        public async Task<ActionResult> Registrar(UserRegisterViewModel viewModel)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState.Values.SelectMany(e => e.Errors));
+
+            var result = await _userManagementService.Create(viewModel).ConfigureAwait(true);
+
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+            viewModel.Senha = string.Empty;
+            viewModel.ConfirmaSenha = string.Empty;
+
+            return Ok(viewModel);
+        }
+
+        [HttpPost("Entrar")]
+        public async Task<ActionResult> Login(UserLoginViewModel viewModel)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState.Values.SelectMany(e => e.Errors));
+
+            var result = await _signInManager.PasswordSignInAsync(viewModel.Email, viewModel.Senha, false, true).ConfigureAwait(true);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest("Usuário ou senha inválido.");
+            }
+
+            return Ok(_userManagementService.GerarJWT(viewModel.Email));
+        }
     }
 }
