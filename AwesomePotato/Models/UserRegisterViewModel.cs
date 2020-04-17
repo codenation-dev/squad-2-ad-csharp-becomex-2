4d4bb336c09b37bf1dc9d4e5ce68d3d74175a949
Luiz Soares <dev.soares@outlook.com>
2020-04-16 22:25:18 -0300

    Added UserRegisterViewModel.cs

---
+++ AwesomePotato/Models/UserRegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AwesomePotato.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage = "O campo {0} é Obrigatório!")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é Obrigatório!")]
+        [StringLength(100, ErrorMessage = "O campo deve conter entre {2} e {1} caracteres", MinimumLength = 10)]
+        public string Senha { get; set; }
+
+        [Compare("Senha", ErrorMessage = "As senhas não conferem.")]
+        public string ConfirmaSenha { get; set; }
+    }
+}
