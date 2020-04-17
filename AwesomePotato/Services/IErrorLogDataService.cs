4315481d1f381dec86ecd127f1167022578bfc04
Luiz Soares <dev.soares@outlook.com>
2020-04-17 13:41:50 -0300

    Removed FilterByToken method

--- AwesomePotato/Services/IErrorLogDataService.cs
+++ AwesomePotato/Services/IErrorLogDataService.cs
@@ -9,10 +9,9 @@ namespace AwesomePotato.Services
     public interface IErrorLogDataService
     {
         ErrorLogData FindById(int id);
-        IList<ErrorLogData> FilterByApplicationTokenLevelDate(string applicationName, string token, int? level, string startDate, string endDate);
+        IList<ErrorLogData> FilterByApplicationLevelDate(string applicationName, int? level, string startDate, string endDate);
         IList<ErrorLogData> GetAll();
         IList<ErrorLogData> FilterByApplication(IList<ErrorLogData> logDatas, string application);
-        IList<ErrorLogData> FilterByToken(IList<ErrorLogData> logDatas, string token);
         IList<ErrorLogData> FilterByLevel(IList<ErrorLogData> logDatas, int? level);
         IList<ErrorLogData> FilterByDate(IList<ErrorLogData> logDatas, string startDate, string endDate);
         ErrorLogData Save(ErrorLogData errorLogData);
